c4eb85f89f1eb74b5b4a48e3d5857456526a0eaf
alexeyglinkin <aleksey.glinkin@skillbox.ru>
2024-06-09 15:52:51 +0300

    Вебхуки

--- check_bot/main.py
+++ check_bot/main.py
@@ -22,24 +22,25 @@ def webhook():
     return 'ok', 200
 
 def set_webhook():
-    response = requests.get(f'https://api.telegram.org/bot{BOT_TOKEN}/setWebhook?url={WEBHOOK_URL}/webhook_report')
+    response = requests.get(f'https://api.telegram.org/bot{BOT_TOKEN}/setWebhook?url={WEBHOOK_URL}/webhook_check_bot')
     print(response.json())
 
 def webhook_thread():
-    while True:
+    for i in range(1, 4):
         set_webhook()
-        time.sleep(30)
+        time.sleep(10)
 set_webhook()
 
-# def delete_webhook():
-#     response = requests.get(f'https://api.telegram.org/bot{BOT_TOKEN}/deleteWebhook')
-#     print(response.json())
+def delete_webhook():
+    response = requests.get(f'https://api.telegram.org/bot{BOT_TOKEN}/deleteWebhook')
+    print(response.json())
 
 if __name__ == "__main__":
+    delete_webhook()
     bot.add_custom_filter(StateFilter(bot))
     set_default_commands(bot)
     # bot.infinity_polling()
 
     webhook_thread = threading.Thread(target=webhook_thread)
     webhook_thread.start()
-    app.run(host='0.0.0.0', port=5005)
+    app.run(host='0.0.0.0', port=5007)
